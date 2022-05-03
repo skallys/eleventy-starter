f1f17e53e25a7af5a24338736be3a705f20afc91
Simon Pinkas <simon@code-think.com>
2022-05-03 17:46:56 +0200

    Remove JS

--- src/js/script.js
+++
@@ -1,5 +0,0 @@
-import { random } from "lodash";
-
-let r = random(0, 100);
-document.querySelector("p").innerHTML = `J'ai utilisé la fonction random() de 
-la librairie lodash, pour générer le chiffre <strong>${r}</strong>. Sympa!`
